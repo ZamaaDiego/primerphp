c2f4fa3aa6c242f3f529e86a0fb1dcc029d5058f
ZamaaDiego <125288492+ZamaaDiego@users.noreply.github.com>
2023-05-18 11:15:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ejemplo</title>
+</head>
+<body>
+
+<?php
+            echo "Tirar por la borda el último intento
+            sólo para ver si flota...
+            ... o se hunde";
+        ?>
+
+</body>
+</html>
